69f7555831f09bad669a317460cdea58ddbcf79a
Rongeard Benoit <benoit.rongeard@gmail.com>
2024-09-28 11:30:09 +0200

    Update tab2 page and tabs page
    - Update tab2 page title to "Photo gallery"
    - Add a camera button to the tab2 page
    - Update tab2 tab button label to "Photos"
    - Add the "images" icon to the tab2 tab button

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { camera } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [
+    IonIcon,
+    IonFabButton,
+    IonFab,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    ExploreContainerComponent,
+  ],
 })
 export class Tab2Page {
-
-  constructor() {}
-
+  constructor() {
+    addIcons({ camera });
+  }
 }
