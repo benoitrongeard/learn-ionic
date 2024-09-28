81f7331ef09d2d9fd32947d3c437f9d8591ea168
Rongeard Benoit <benoit.rongeard@gmail.com>
2024-09-28 11:37:51 +0200

    Add camera service

---
+++ src/app/services/camera.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CameraService {
+  constructor() {}
+
+  // public async addNewToGallery() {
+  //   // Take a photo
+  //   const capturedPhoto = await Camera.getPhoto({
+  //     resultType: CameraResultType.Uri,
+  //     source: CameraSource.Camera,
+  //     quality: 100,
+  //   });
+  // }
+}
